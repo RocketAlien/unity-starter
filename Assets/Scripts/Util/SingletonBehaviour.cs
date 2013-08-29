a5970aab9f260070e6e563bd8387157a3bd6626b
Dmitri Bouianov <dbouianov@gmail.com>
2013-08-29 12:07:28 -0700

    Adding Persistent editor parameter to SingletonBehaviour

--- Assets/Scripts/Util/SingletonBehaviour.cs
+++ Assets/Scripts/Util/SingletonBehaviour.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 //turn any subclass into a singleton behaviour
 public class SingletonBehaviour<T> : BaseBehaviour where T : BaseBehaviour {
 
-    private static T instance;
-    public static T Instance {
-        get {
-            if (instance == null) {
-                instance = FindObjectOfType<T>();
-            }
-            return instance;
+    public bool Persistent = false;
+
+    public static T Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance != null) {
+            //destroy game object that persisted across scenes
+            if (Persistent) Destroy(gameObject);
+        }
+        else {
+            if (Persistent) DontDestroyOnLoad(gameObject);
+            Instance = this as T;
         }
     }
 }
